73d3a8df9bf83d815872be8e29e25623d204db10
Mamikie Maemu <maemu.ronewa98@gmail.com>
2025-09-14 14:35:08 +0200

    Created a new file called 1-multi_language.js and added 3 language variable then console logged it and ran the code with node.

---
+++ 1-multi_language.js
@@ -0,0 +1,7 @@
+let firstLanguange = "C is fun";
+let secondLanguange = "Python is cool";
+let thirdLanguange = "JavaScript is amazing";
+
+console.log(firstLanguange);
+console.log(secondLanguange);
+console.log(thirdLanguange);
