4221eff4116eec7ca6afc4584fcb6cfb2548efb7
Mamikie Maemu <maemu.ronewa98@gmail.com>
2025-09-14 14:45:47 +0200

    Worked on building a conditional statement using arguments

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+const args = process.argv.length = 1;
+
+if (args === 0) {
+  console.log('No argument');
+} else if (args === 1) {
+  console.log('Arguments found');
+} else {
+  console.log('Arguments found')};
