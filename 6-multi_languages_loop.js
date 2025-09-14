1e2c76ff7c010d8a8d90c76d6d038cd86d77b802
Mamikie Maemu <maemu.ronewa98@gmail.com>
2025-09-14 15:41:05 +0200

    Added a for loop to go throught the languages in an array

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-languages = [C is fun, Python is cool, JavaScript is amazing];
+languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < languages.length; i++) {
     console.log(languages[i]);
