f91d525dd6fd96f69ebdcfedd9269cd687f50f37
Mamikie Maemu <maemu.ronewa98@gmail.com>
2025-09-14 15:02:23 +0200

    Switched my zero to undefined and actually fixed the errors

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,5 +1,5 @@
-const argument = process.argv.slice(2);
-if (argument === 0) {
+const argument = process.argv[2];
+if (argument === undefined) {
     console.log('No argument');
 } else {
     console.log(argument);
